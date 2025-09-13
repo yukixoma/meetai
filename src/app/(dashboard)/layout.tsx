6e6f7253eb8b6d3bf9a961eec2d96dfb6036e1c6
yukixoma08@gmail.com <yukixoma08@gmail.com>
2025-09-13 10:28:04 +0900

    07: dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,14 @@
-import { ReactNode } from "react";
-
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <SidebarProvider>
             <DashboardSidebar />
             <main className="flex flex-col h-screen w-screen bg-muted">
+                <DashboardNavbar />
                 {children}
             </main>
         </SidebarProvider>
