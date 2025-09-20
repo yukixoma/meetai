40c1b0dee4a76b1fb471710458575d2b1b985e02
yukixoma08@gmail.com <yukixoma08@gmail.com>
2025-09-20 15:41:51 +0900

    20: Meeting page

--- src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,5 +1,45 @@
-function Page() {
-    return <div>Meeting ID page</div>;
+import { Suspense } from "react";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { ErrorBoundary } from "react-error-boundary";
+
+import { authenticator } from "@/components/authenticator";
+
+import {
+    MeetingIdView,
+    MeetingIdViewErrorState,
+    MeetingIdViewLoadingState,
+} from "@/modules/meetings/ui/views/meeting-id-view";
+
+interface MeetingIdPageProps {
+    params: Promise<{ meetingId: string }>;
 }
+
+const Page = async ({ params }: MeetingIdPageProps) => {
+    /** Security checking */
+    await authenticator();
+
+    const { meetingId } = await params;
+    const queryClient = getQueryClient();
+    await queryClient.prefetchQuery(
+        trpc.meetings.getOne.queryOptions({
+            id: meetingId,
+        })
+    );
+    //TOD: prefetch get transcript "meeting.getTranscript"
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<MeetingIdViewLoadingState />}>
+                <ErrorBoundary fallback={<MeetingIdViewErrorState />}>
+                    <MeetingIdView {...{ meetingId }} />
+                </ErrorBoundary>
+            </Suspense>
+        </HydrationBoundary>
+    );
+};
 
 export default Page;
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -29,53 +29,6 @@ export const agentsRouter = createTRPCRouter({
             return createdAgent;
         }),
 
-    update: protectedProcedure
-        .input(agentsUpdateSchema)
-        .mutation(async ({ input, ctx }) => {
-            const [updatedAgent] = await db
-                .update(agents)
-                .set(input)
-                .where(
-                    and(
-                        eq(agents.id, input.id),
-                        eq(agents.userId, ctx.auth.user.id)
-                    )
-                )
-                .returning();
-
-            if (!updatedAgent) {
-                throw new TRPCError({
-                    code: "NOT_FOUND",
-                    message: "Agent not found",
-                });
-            }
-
-            return updatedAgent;
-        }),
-
-    remove: protectedProcedure
-        .input(z.object({ id: z.string() }))
-        .mutation(async ({ ctx, input }) => {
-            const [removedAgent] = await db
-                .delete(agents)
-                .where(
-                    and(
-                        eq(agents.id, input.id),
-                        eq(agents.userId, ctx.auth.user.id)
-                    )
-                )
-                .returning();
-
-            if (!removedAgent) {
-                throw new TRPCError({
-                    code: "NOT_FOUND",
-                    message: "Agent not found",
-                });
-            }
-
-            return removedAgent;
-        }),
-
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
         .query(async ({ input, ctx }) => {
@@ -146,5 +99,52 @@ export const agentsRouter = createTRPCRouter({
             const totalPages = Math.ceil(total / pageSize);
 
             return { items, total, totalPages };
+        }),
+
+    update: protectedProcedure
+        .input(agentsUpdateSchema)
+        .mutation(async ({ input, ctx }) => {
+            const [updatedAgent] = await db
+                .update(agents)
+                .set(input)
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+
+            if (!updatedAgent) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Agent not found",
+                });
+            }
+
+            return updatedAgent;
+        }),
+
+    remove: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const [removedAgent] = await db
+                .delete(agents)
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+
+            if (!removedAgent) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Agent not found",
+                });
+            }
+
+            return removedAgent;
         }),
 });
